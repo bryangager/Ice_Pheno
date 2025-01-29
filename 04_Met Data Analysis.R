9601a0480c2fab5367608a3faf13f984227953fa
bryangager <bryan.gager@colorado.edu>
2025-01-29 12:08:02 -0700

    met data more, messing with figures

--- 04_Met Data Analysis.R
+++ 04_Met Data Analysis.R
@@ -28,108 +28,63 @@ weather_daily <- weatherData %>%
   ) %>% addWaterYear()
 ## Now I'll calculate the cumulative temperature of different time windows
 # CHANGE THESE DATES TO MAKE EACH DATAFRAME
-start_month_day <- "04-01"
-end_month_day <- "06-01"
+start_month_day <- "03-01"
+end_month_day <- "05-01"
 
 # Add year-based grouping and calculate cumulative sums
-april_may_cumul_temp <- weather_daily %>%
+march_april_cumul_temp <- weather_daily %>%
   mutate(
     year = format(Date, "%Y"),                     # Extract the year
     month_day = format(Date, "%m-%d")              # Extract month and day
   ) %>%
   filter(month_day >= start_month_day & month_day < end_month_day) %>% # Filter by date window
-  group_by(year) %>%                               # Group by year
-  mutate(
-    T_air_2_m_AprilMaycumul = cumsum(T_air_2_m_mean),     # Cumulative sum for T_air_2_m_mean
-    T_air_6_m_AprilMaycumul = cumsum(T_air_6_m_mean)      # Cumulative sum for T_air_6_m_mean
-  ) %>%
-  ungroup()  
+  group_by(waterYear) %>%                               # Group by year
+  # mutate(
+  #   T_air_2_m_AprilMaycumul = cumsum(T_air_2_m_mean),     # Cumulative sum for T_air_2_m_mean
+  #   T_air_6_m_AprilMaycumul = cumsum(T_air_6_m_mean)      # Cumulative sum for T_air_6_m_mean
+  # ) %>%
+  summarize(
+    T_MarAprcumul = sum(T_air_2_m_mean)     # Cumulative sum for T_air_2_m_mean
+    # T_air_6_m_AprilMaycumul = sum(T_air_6_m_mean)      # Cumulative sum for T_air_6_m_mean
+  ) 
+  # ungroup()  %>%
+  # distinct(., waterYear, T_air_2_m_AprilMaycumul, .keep_all = FALSE)
 
-march_cumul_temp %>% 
-  ggplot(aes(x=Date, y=T_air_2_m_Marchcumul))+
-  geom_point()+
-  facet_wrap(.~year, scales="free_x")
+monthly_cumul_temp <- weather_daily %>%
+  mutate(month = month(Date, label=TRUE)) %>%
+  filter(month %in% c("Feb","Mar","Apr","May")) %>%
+  group_by(waterYear, month) %>%
+  summarize(T_ = sum(T_air_2_m_mean)) %>%
+  pivot_wider(names_from = "month", values_from = "T_",
+              names_glue = "{.value}_{month}")
 
-# joining them all in probably the worst way possible
-cumulativeAirTemp <- full_join(march_cumul_temp,april_cumul_temp,by=c("Date","year")) %>% 
-  select(Date,year,waterYear.x,T_air_2_m_mean.x,T_air_2_m_Marchcumul,T_air_2_m_Aprilcumul) %>%
-  rename(c(waterYear=waterYear.x,T_air_2_m_mean=T_air_2_m_mean.x))
-
-cumulativeAirTemp1 <- full_join(cumulativeAirTemp,may_cumul_temp,by=c("Date","year")) %>% 
-  select(Date,year,waterYear.x,T_air_2_m_mean.x,T_air_2_m_Marchcumul,T_air_2_m_Aprilcumul,T_air_2_m_Maycumul) %>%
-  rename(c(waterYear=waterYear.x,T_air_2_m_mean=T_air_2_m_mean.x))
-
-cumulativeAirTemp2 <- full_join(cumulativeAirTemp1,march_april_cumul_temp,by=c("Date","year")) %>% 
-  select(Date,year,waterYear.x,T_air_2_m_mean.x,T_air_2_m_Marchcumul,T_air_2_m_Aprilcumul,T_air_2_m_Maycumul,T_air_2_m_MarchAprilcumul) %>%
-  rename(c(waterYear=waterYear.x,T_air_2_m_mean=T_air_2_m_mean.x))
-
-cumulativeAirTemp3 <- full_join(cumulativeAirTemp2,march_may_cumul_temp,by=c("Date","year")) %>% 
-  select(Date,year,waterYear.x,T_air_2_m_mean.x,T_air_2_m_Marchcumul,T_air_2_m_Aprilcumul,T_air_2_m_Maycumul,T_air_2_m_MarchAprilcumul,T_air_2_m_MarchMaycumul) %>%
-  rename(c(waterYear=waterYear.x,T_air_2_m_mean=T_air_2_m_mean.x))
-
-cumulativeAirTemp4 <- full_join(cumulativeAirTemp3,april_may_cumul_temp,by=c("Date","year")) %>% 
-  select(Date,year,waterYear.x,T_air_2_m_mean.x,T_air_2_m_Marchcumul,T_air_2_m_Aprilcumul,T_air_2_m_Maycumul,T_air_2_m_MarchAprilcumul,T_air_2_m_MarchMaycumul,T_air_2_m_AprilMaycumul) %>%
-  rename(c(waterYear=waterYear.x,T_air_2_m_mean=T_air_2_m_mean.x))
-
-cumulativeAirTempAll <- cumulativeAirTemp4 %>% mutate(year = as.integer(year))
+april_may_cumul_temp %>% 
+  ggplot(aes(x=waterYear, y=T_AprMaycumul))+
+  geom_point()
 
 # hooray! now we have mean air temp and cumulative air temp for many different windows. Let's merge it all together:
-airTemp_hindcast_SWE <- full_join(cumulativeAirTempAll,hindcast_SWE,by="year") %>% rename(waterYear=waterYear.x) %>% select(-c(first_snow_acc,first_snow_acc_doy,cont_snow_acc,cont_snow_acc_doy,first_snow_melt,first_snow_melt_doy,last_snow_melt,last_snow_melt_doy,waterYear.y,first_snow_acc_wydoy,cont_snow_acc_wydoy,first_snow_melt_wydoy,last_snow_melt_wydoy,max_swe_date))
-airTemp_hindcast_SWE$month <- format(airTemp_hindcast_SWE$Date, "%m")
 
-# now we have a messy dataframe that has everything in it, let's run some models and see what happens.
-###### MODELS AND CORRELATIONS:
-tempModel <- lm(predicted_ice_off_wy_doy~T_air_2_m_mean, data = airTemp_hindcast_SWE)
-summary(tempModel)
+met_full <- april_may_cumul_temp %>%
+  left_join(., monthly_cumul_temp, by = "waterYear") %>%
+  left_join(., hindcast_SWE %>% select(contains("wydoy"),max_swe,waterYear,predicted_ice_off_wy_doy), by ="waterYear")
 
-tempCumulMarchModel <- lm(predicted_ice_off_wy_doy~T_air_2_m_Marchcumul, data = airTemp_hindcast_SWE)
-summary(tempCumulMarchModel)
+met_full <- left_join(met_full,march_may_cumul_temp,by="waterYear")
 
-# This doesn't work because of the NAs?
-ggpairs(airTemp_hindcast_SWE)+theme_bw()
+met_full <- left_join(met_full,march_april_cumul_temp,by="waterYear")
 
-ggpairs(airTemp_hindcast_SWE, columns=c(4,14))+theme_bw()
+met_full <- met_full %>% select(c("waterYear","predicted_ice_off_wy_doy","T__Feb","T__Mar","T__Apr","T__May","T_MarAprcumul.x","T_AprMaycumul","T_MarMaycumul","first_snow_acc_wydoy","cont_snow_acc_wydoy","first_snow_melt_wydoy","last_snow_melt_wydoy","max_swe_wydoy","max_swe"))
+# This is the final df with the temperatures, snow, and hindcasted dates.
+met_full <- met_full %>% rename(c(T_MarAprcumul=T_MarAprcumul.x,hindcast_ice_off_wy_doy=predicted_ice_off_wy_doy))
 
-## Statistically significant correlations: April Cumulative Temp (-0.361***), May Cumulative Temp: (-0.299***),
-## March-April Cumulative Temp (-0.089***), March-May Cumulative Temperature (-0.227***), April-May Cumulative Temp (-0.474***)
-## Max SWE (0.406***), Max SWE doy (0.551***)
+##### Correlations
 
-## The only variables that were not statistically correlated with the predicted date of ice-off 
-## were Mean 2m Air Temp and March Cumulative Temp
 
-###### Change Over Time:
-# March Cumul Temp
-marchPlot <-
-  ggplot(airTemp_hindcast_SWE,aes(x=Date,y=T_air_2_m_Marchcumul))+
-  geom_point()+
-  geom_smooth(method=lm)
-marchPlot
-# April Cumul Temp
-aprilPlot <-
-  ggplot(airTemp_hindcast_SWE,aes(x=Date,y=T_air_2_m_Aprilcumul))+
-  geom_point()+
-  geom_smooth(method=lm)
-aprilPlot
-# May Cumul Temp
-mayPlot <-
-  ggplot(airTemp_hindcast_SWE,aes(x=Date,y=T_air_2_m_Maycumul))+
-  geom_point()+
-  geom_smooth(method=lm)
-mayPlot
-# March- May Cumul Temp:
-marchMayPlot <-
-  ggplot(airTemp_hindcast_SWE,aes(x=Date,y=T_air_2_m_MarchMaycumul))+
-  geom_point()+
-  geom_smooth(method=lm)
-marchMayPlot
-# Mean Daily Temp over time:
-weather_daily$month <- format(weather_daily$Date, "%m") %>% as.integer(weather_daily$month)
+ggpairs(met_full)+theme_bw()
 
-dailyTempPlot <- 
-  ggplot(weather_daily %>% filter(month>=3&month<=5),aes(x=month,y=T_air_2_m_mean))+
-  geom_point()+
-  facet_wrap(~waterYear)
-dailyTempPlot
+ggpairs(met_full %>% select(T_AprMaycumul,T_MarAprcumul,T_MarMaycumul,T__Apr,T__May,
+                            first_snow_melt_wydoy, last_snow_melt_wydoy,
+                            max_swe_wydoy, max_swe,hindcast_ice_off_wy_doy))+theme_bw()
+
 
 ##### 20th Percentile Dates:
 
@@ -152,18 +107,21 @@ cumul_dis_percentiles <- cumulative_dat_ungroup %>%
   group_by(waterYear) %>%
   group_modify(~ find_percentiles(.x, c(0.2, 0.5, 0.8)))
 
-percentile_hindcast_dates <- left_join(cumul_dis_percentiles,hindcasted_dates,by="waterYear") %>% rename(wy_doy_perc=wy_doy) %>% filter(waterYear!=2024)
+twent_perc_dates <- twent_perc_dates %>% addWaterYear() %>% select(c(waterYear,wy_doy_20Percent))
 
-# Correlation plots
-ggpairs(percentile_hindcast_dates %>% filter(percentile==0.2) %>% select(-percentile)) # corr: 0.414**
+#### Join 20th percentile of cumulative discharge dates with met_full data
+met_full <- left_join(met_full,twent_perc_dates,by="waterYear") 
 
-ggpairs(percentile_hindcast_dates %>% filter(percentile==0.5) %>% select(-percentile)) # corr: 0.288
-
-ggpairs(percentile_hindcast_dates %>% filter(percentile==0.8) %>% select(-percentile)) # corr: 0.023
 
 # Plot of 20th percentile wy_doy over time
-ggplot(percentile_hindcast_dates %>% filter(percentile==0.2), aes(x=waterYear,y=wy_doy_perc))+
+ggplot(twent_perc_dates, aes(x=waterYear,y=wy_doy_20Percent))+
   geom_point()
+
+### Correlation with 20 percetn dates
+ggpairs(met_full %>% select(T_AprMaycumul,T_MarAprcumul,T_MarMaycumul,T__Apr,T__May,
+                            first_snow_melt_wydoy, last_snow_melt_wydoy,
+                            max_swe_wydoy, max_swe,wy_doy_20Percent,hindcast_ice_off_wy_doy))+theme_bw()
+## 20th percent dates woohoo
 
 
 ##### Pierson Dates:
@@ -176,6 +134,6 @@ pierson_hindcast_dates <- left_join(pierson_dates,hindcasted_dates,by="waterYear
 # pierson vs. my model dates
 
 ggplot(pierson_hindcast_dates, aes(x=first_no_ice_wy_doy,y=wy_doy_pierson_off))+
-  geom_point(aes(first_no_ice_wy_doy, color = waterYear))+
-  geom_point(aes(wy_doy_pierson_off, color = waterYear))+
-  scale_color_gradient(low="blue",high="red")
+  geom_point()+
+  scale_color_gradient(low="blue",high="red")+
+  geom_abline(intercept=0, slope=1)
