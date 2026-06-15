394d592966c2397f331efd3ea49137b0c27fc410
Your Name <kgannon59401@gmail.com>
2026-06-15 15:06:56 -0600

    re-org done through hydro data

---
+++ source/00_libraries.R
@@ -0,0 +1,22 @@
+##############################################
+# Libraries 
+##############################################
+
+if (!require('pacman')) install.packages('pacman'); library('pacman')
+
+##Load all the libraries your heart desires
+pacman::p_load(dplyr,
+              dataRetrieval,
+              lubridate,
+              tidyr,
+              ggplot2,
+              viridis,
+              readxl,
+              imputeTS,
+              tsibble,
+              sjPlot,
+              pROC,
+              gridExtra,
+              broom,
+              gtsummary)
+
